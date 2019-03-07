6c7dbb8e5a0d2353c41b34322f2b85683e418d12
2234839456 <2234839456@qq.com>
2019-03-07 09:59:37 +0800

    再编译过程中加上 。html 但没解决年份的问题

--- _themes/one/theme.js
+++ _themes/one/theme.js
@@ -6,5 +6,13 @@ exports.filters = {
     return writers.filter(function(writer) {
       return ~writer.indexOf(name)
     }).length;
+  },
+  description: function(writers, name) {
+    let path=writers.match(/href=".\/(.*)"/)
+    if(path!==null)
+      writers=writers.replace(path[1],path[1]+".html")
+    // console.log(writers);
+    // console.log('----------------------');
+    return writers
   }
 };
