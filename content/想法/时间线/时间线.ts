c5f84d2035ca9c49d5cc716bc97f34addbf3d629
申子龙@暗夜精灵 <2234839456@qq.com>
2019-08-01 15:58:29 +0800

    新增赞助

---
+++ content/想法/时间线/时间线.ts
@@ -0,0 +1,16 @@
+class Node {
+    constructor(
+        /** 节点名 */ public name:string,
+        /** 内容 */ public body:Body,
+        /** 节点的时间id，用这个来区分节点的 */
+        public timeId=Date.now(),) {
+
+    }
+}
+
+interface Body{
+    /** body类型，用以决定怎么解析 */ type:1|2
+    /** 携带的内容 */ contont:any
+}
+
+export const a=1
