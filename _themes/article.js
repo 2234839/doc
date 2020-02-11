25788cd66fdec0a548db90f618473821e750fb37
崮生 <admin@shenzilong.cn>
2020-02-11 15:48:13 +0800

    :fire: 目录新增折叠

--- _themes/article.js
+++ _themes/article.js
@@ -154,14 +154,16 @@ function runCode({ code, lang, el }) {
 (function() {
   "use strict";
   const menu = document.querySelectorAll("h1,h2,h3,h4,h5,h6,h7,h8");
-  const container = document.querySelector("崮生_userjs_导航") || document.createElement("崮生_userjs_导航");
-  container.innerHTML = "";
+  const container = document.querySelector("#崮生_userjs_导航") || document.createElement("details");
+  if (document.querySelector("html").offsetWidth >= 1200) container.setAttribute("open", "");
+  container.id = "#崮生_userjs_导航";
+  container.innerHTML = `<summary>目录</summary>`;
   container.style = `
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  right: 2rem;
-  top: 2rem
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    right: 1rem;
+    top: 1rem
   `;
   document.body.appendChild(container);
 
