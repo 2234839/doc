c209506d3ba9bcd09c75810df272cd1343162d44
崮生 <admin@shenzilong.cn>
2020-02-22 14:20:34 +0800

    :white_check_mark: 删除gitalk

--- _themes/article.js
+++ _themes/article.js
@@ -186,16 +186,3 @@ function runCode({ code, lang, el }) {
     a.href = a.href.replace(/\.md$/, ".html");
   });
 })();
-/** gitalk */ (function() {
-  var gitalk = new Gitalk({
-    clientID: "effa7a3abb2f215d80b2",
-    clientSecret: "28963609ab8bdf92405560fb0a82c98912feb0db",
-    repo: "doc",
-    owner: "2234839",
-    admin: ["2234839"],
-    id: location.pathname, // Ensure uniqueness and length less than 50
-    distractionFreeMode: false, // Facebook-like distraction free mode
-  });
-
-  gitalk.render("gitalk-container");
-})();
