bc83a390488db32958f96699e5ded41c8ebe8e5c
崮生 <admin@shenzilong.cn>
2020-05-24 14:00:01 +0800

    feat: :sparkles: 正确的跑通了服务

---
+++ src/routes/[path(.+)].svelte
@@ -0,0 +1,27 @@
+<script context="module" lang="ts">
+  import { preload as p1 } from "./path.ts";
+  export const preload = p1;
+</script>
+
+<script>
+  import { onMount } from "svelte";
+  export let html;
+  export let title;
+
+  onMount(async () => {
+    console.log("开始加载 article");
+    import("../../_themes/article.ts").then((r) => {
+      console.log(r);
+      r.run();
+    });
+    // console.log(article_js);
+    // article_js.run();
+  });
+  console.log(222222);
+</script>
+
+<svelte:head>
+  <title>{title} - 崮生</title>
+</svelte:head>
+
+{@html html}
---
+++ src/routes/path.ts
@@ -0,0 +1,28 @@
+// import { try_pathToMd } from "../lib/md-parser";
+
+export async function preload(this: context, page: page, session: any) {
+  console.log(page.path);
+
+  const res = await this.fetch(`https://shenzilong.cn${page.path.replace(/\.html/, ".json")}`);
+  // try_pathToMd(page.path);
+  if (!res.ok) {
+    /** 报错 404 */
+    return this.error(404, "Not found");
+  }
+
+  const article = await res.json();
+  return { time: Date.now(), page, html: article.raw_html, title: article.title };
+}
+
+type context = {
+  fetch: typeof fetch;
+  error: (arg0: number, arg1: string) => any;
+};
+type page = {
+  host: /** "localhost:3000"  */ string;
+  path: /** "/正则.md" */ string;
+  query: /** {} */ any;
+  params: /**  {
+    path: "正则.md";
+  } */ any;
+};
