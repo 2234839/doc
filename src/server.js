a5177695d48bea969bb91385c249e8344ade3d32
崮生 <admin@shenzilong.cn>
2020-05-24 15:50:15 +0800

    fix: :bug: 简单的解决文件路径问题

--- src/server.js
+++ src/server.js
@@ -1,17 +1,44 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
-
+import sirv from "sirv";
+import polka from "polka";
+import compression from "compression";
+import * as sapper from "@sapper/server";
+import path from "path";
+import fs from "fs";
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
+const server_path = __dirname;
+const root_path = path.resolve(__dirname, "../../../");
+const client_path = path.resolve(server_path, "../client");
+console.log({ root_path, __dirname, client_path });
+function sendFile(filePath, res) {// 这里到时候再完善
+  if (filePath.endsWith(".js")) {
+    res.setHeader("content-type", "application/javascript; charset=utf-8");
+  }
+  fs.createReadStream(filePath).pipe(res);
+}
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(function file_server(req, res, next) {
+    const file_path = path.resolve(root_path, "./" + req.url);
+    if (req.method === "GET") {
+      fs.exists(file_path, (r) => {
+        if (r) {
+          sendFile(file_path, res);
+        } else {
+          if (/\/client\//.test(req.url)) {
+            const fileName = req.url.replace(/.*\/client\//, "");
+            const client_file_path = path.resolve(client_path, "./" + fileName);
+            sendFile(client_file_path, res);
+          } else {
+            next(); // move on
+          }
+        }
+      });
+    } else {
+      next(); // move on
+    }
+  })
+  .use(compression({ threshold: 0 }), sirv("static", { dev }), sapper.middleware())
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
