189ea370cdeadf2feef00561ed524267ca487240
崮生 <admin@shenzilong.cn>
2020-11-02 12:00:26 +0800

    ✨feat(sitemap): 新增 sitemap 接口

--- src/lib/sitemap.ts
+++ src/lib/sitemap.ts
@@ -1,3 +1,35 @@
-export function generateSiteMap(){
+import { doc_path } from "./env";
+import { 获取文档资源 } from "./资源检索/最近更新";
 
+export const siteMap = generateSiteMap();
+
+export async function generateSiteMap() {
+  const r = (await 获取文档资源()).md_file.map((el) => ({ ...el, 相对路径: el.fullSrc.replace(doc_path, "") }));
+  const menu = (await 获取文档资源()).allFile
+    .filter((el) => el.isDirectory)
+    .map((el) => ({ ...el, 相对路径: el.fullSrc.replace(doc_path, "") }));
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${r
+      .map((el) => {
+        return `
+            <url>
+                <loc>${el.相对路径.slice(/** 去除 .md  */ 0, -3).replace(/[\\\/]/g, "/")}.html</loc>
+                <lastmod>${new Date(el.mtimeMs).toISOString()}</lastmod>
+            </url>
+        `;
+      })
+      .join("")}
+      ${menu
+        .map((el) => {
+          return `
+              <url>
+                  <loc>${el.相对路径.replace(/[\\\/]/g, "/")}/</loc>
+                  <lastmod>${new Date(el.mtimeMs).toISOString()}</lastmod>
+              </url>
+          `;
+        })
+        .join("")}
+</urlset>`;
 }
--- src/lib/资源检索/最近更新.ts
+++ src/lib/资源检索/最近更新.ts
@@ -2,10 +2,12 @@ import * as fs from "fs/promises";
 import * as Path from "path";
 import { doc_path } from "../env";
 
-export async function 获取项目下所有文件(src:string) {
+let oldTime = Date.now();
+
+export async function 获取项目下所有文件(src: string) {
   // 读取目录中的所有文件/目录
   const paths = await fs.readdir(src);
-  const files = [] as { fullSrc: string; mtimeMs: number }[];
+  const files = [] as { fullSrc: string; mtimeMs: number; isDirectory: boolean }[];
   for (const path of paths) {
     //拼合路径
     const fullSrc = Path.resolve(src + "/" + path);
@@ -13,10 +15,11 @@ export async function 获取项目下所有文件(src:string) {
     const st = await fs.stat(fullSrc);
     // 判断是否为文件
     if (st.isFile()) {
-      files.push({ fullSrc, mtimeMs: st.mtimeMs });
+      files.push({ fullSrc, mtimeMs: st.mtimeMs, isDirectory: false });
     }
     // 如果是目录则递归调用自身
     else if (st.isDirectory()) {
+      files.push({ fullSrc, mtimeMs: st.mtimeMs, isDirectory: true });
       files.push(...(await 获取项目下所有文件(fullSrc)));
     }
   }
@@ -25,9 +28,25 @@ export async function 获取项目下所有文件(src:string) {
 
 export const 最近更新 = [];
 
-async function test() {
+async function main() {
   const allFile = await 获取项目下所有文件(doc_path);
-  const md_file = allFile.filter((el) => el.fullSrc.endsWith(".md")).sort((a, b) => b.mtimeMs - a.mtimeMs);
-  // console.log("[md_file]", md_file.slice(0, 10));
+  const md_file = allFile
+    .filter((el) => !el.isDirectory && el.fullSrc.endsWith(".md"))
+    .sort((a, b) => b.mtimeMs - a.mtimeMs);
+  return {
+    allFile,
+    md_file,
+  };
 }
-test();
+export let 文档资源 = main();
+
+/** 会触发更新资源的流程 */
+export function 获取文档资源() {
+  if (Date.now() - oldTime > 24 * 60 * 60 * 1000) {
+    oldTime = Date.now();
+    setTimeout(() => {
+      文档资源 = main();
+    }, 100);
+  }
+  return 文档资源;
+}
