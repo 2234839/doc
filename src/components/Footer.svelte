d292a93199d25ad308a2740fd0c0c0984d7dd804
崮生 <admin@shenzilong.cn>
2020-11-06 21:28:49 +0800

    ✨feat(feed): 修改 feed 依据文件修改时间产生

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -48,9 +48,7 @@
   <footer class="row">
     <span>崮生 • 一些随笔 🎨</span>
     <span>copyright © 2018.11.6 - {new Date().getFullYear()}.{new Date().getMonth() + 1}</span>
-    <a href="https://shenzilong.cn/blog/feed">
-      <img class="ml-1 h-3" src="/static/_themes/feed.png" alt="rss订阅" title="rss订阅" />
-    </a>
+    <a href="/blog/feed"> <img class="ml-1 h-3" src="/static/_themes/feed.png" alt="rss订阅" title="rss订阅" /> </a>
     <a href="https://beian.miit.gov.cn">湘ICP备18021783号</a>
     <a href="https://github.com/2234839/doc">GIT HUB</a>
   </footer>
---
+++ src/routes/blog/feed.ts
@@ -0,0 +1,28 @@
+import { API } from "../../lib/api/fetch";
+
+/** post 接口做预览之用 */
+export async function get(req: any, res: any) {
+  const xml = `<?xml version="1.0" encoding="utf8" ?>
+    <rss version="2.0">
+    <channel>
+      <title>崮生的笔记</title>
+      <link>https://shenzilong.cn</link>
+      <description>崮生的笔记</description>
+      ${(await API.获取最近更新())
+        .map((el) => {
+          return `
+                <item>
+                <title>${el.name}</title>
+                <link>${el.webPath}</link>
+                <description>${/** TODO 这里应该要替换为真正的描述 */ el.name}</description>
+            </item>
+            `;
+        })
+        .join("")}
+    </channel>
+    </rss>`;
+  res.writeHead(200, {
+    "Content-Type": "application/xml",
+  });
+  res.end(xml);
+}
