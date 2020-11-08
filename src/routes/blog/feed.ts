4853e6b7ae4712f5e50b2ce002e9d90c066331d0
崮生 <admin@shenzilong.cn>
2020-11-08 17:23:59 +0800

    🎈perf(feed): 添加html内容的feed

--- src/routes/blog/feed.ts
+++ src/routes/blog/feed.ts
@@ -1,27 +1,30 @@
 import { siteBaseUrl } from "../../data/app.config";
 import { API } from "../../lib/api/fetch";
+import { lute, 去除思源笔记id的路径 } from "../../lib/md解析/lute";
+import { 获取文档资源 } from "../../lib/资源检索/最近更新";
 
 /** post 接口做预览之用 */
 export async function get(req: any, res: any) {
   const xml = `<?xml version="1.0" encoding="utf8" ?>
-    <rss version="2.0">
-    <channel>
-      <title>崮生的笔记</title>
-      <link>https://shenzilong.cn</link>
-      <description>崮生的笔记</description>
-      ${(await API.获取最近更新())
+    <feed version="2.0">
+    <title>崮生的笔记</title>
+    <link>https://shenzilong.cn</link>
+    <description>崮生的笔记</description>
+      ${(await 获取文档资源()).md_file
+        .slice(0, 20)
         .map((el) => {
-          return `
-                <item>
-                <title>${el.name}</title>
+          return `<entry>
+                <title>${el.isDirectory ? el.basename : 去除思源笔记id的路径(el.basename)}</title>
                 <link>${siteBaseUrl}${el.webPath}</link>
-                <description>${/** TODO 这里应该要替换为真正的描述 */ el.name}</description>
-            </item>
+                <content type="html" xml:lang="zh" xml:base="${siteBaseUrl}">
+
+                <![CDATA[${lute.MarkdownStr("", el.mdStr)}
+              ]]></content>
+            </entry>
             `;
         })
         .join("")}
-    </channel>
-    </rss>`;
+    </feed>`;
   res.writeHead(200, {
     "Content-Type": "application/xml",
   });
