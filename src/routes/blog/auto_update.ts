ecb481fd532ba063d6ec91b90123758e8a09e908
崮生 <admin@shenzilong.cn>
2020-12-24 19:57:53 +0800

    新增编译 html 的代码

--- src/routes/blog/auto_update.ts
+++ src/routes/blog/auto_update.ts
@@ -13,6 +13,13 @@ export async function get(req: any, res: any) {
       console.log("[更新代码博客失败]", e);
     } else {
       console.log("[更新成功]");
+      exec(`../app/md2website/linux_md2website "/root/doc/doc" "./docHTML" "/root/app/md2website/views/"`, { cwd: root_path }, (e, stdout) => {
+        if (e) {
+          console.log("[编译html失败]", e);
+        } else {
+          console.log("[编译html成功]");
+        }
+      });
       /** 去触发一次更新 */
       获取文档资源(true);
     }
