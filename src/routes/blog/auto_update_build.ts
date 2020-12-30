d94745f693d05573e8fe5ee39634b3992eba1260
崮生 <admin@shenzilong.cn>
2020-12-31 05:39:54 +0800

    ✨feat(auto update): 自动更新程序

---
+++ src/routes/blog/auto_update_build.ts
@@ -0,0 +1,30 @@
+import { exec } from "child_process";
+import { root_path } from "../../lib/env";
+/** post 接口做预览之用 ldsfoiu9071384ohjfaiou149814 */
+export async function get(req: any, res: any) {
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+  res.end(JSON.stringify({ code: 200 }));
+  /** 更新代码，刷新博客内容 */
+  exec("cd /root/doc/__sapper__/doc/ && git pull", { cwd: root_path }, (e, stdout) => {
+    if (e) {
+      console.log("[更新程序失败]", e);
+    } else {
+      console.log("[更新程序成功]");
+      exec(
+        `cd /root/doc/__sapper__/doc/ && \\cp -rf ./build/ ../build/ && pm2 reload blog`,
+        { cwd: root_path },
+        (e, stdout) => {
+          if (e) {
+            console.log("[启动程序失败]", e);
+          } else {
+            console.log("[启动程序成功]", stdout.toString());
+          }
+        },
+      );
+    }
+  });
+}
+
+export const post = get;
