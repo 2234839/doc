86b0119b53cfaaa7c63ced15a193c440e0fcfb89
崮生 <admin@shenzilong.cn>
2020-12-31 15:20:02 +0800

    🎈perf(404): 新增提示

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -46,6 +46,7 @@
 <h1 class="text-red-800">{status}</h1>
 
 <p>{error.message}</p>
+<p>对于一些资源页面，例如某某网页备份这里 404 可以尝试再刷新一遍</p>
 
 {#if dev && error.stack}
   <pre>{error.stack}</pre>
@@ -57,7 +58,9 @@
   <h3>你要找的是下面这些吗？</h3>
   <div class="flex flex-wrap">
     {#each list as item}
-      <div class="w-full lg:w-1/5 md:w-1/4 sm:w-1/2"><a href={item.webPath}>{item.name}</a></div>
+      <div class="w-full lg:w-1/5 md:w-1/4 sm:w-1/2">
+        <a href={item.webPath}>{item.name}</a>
+      </div>
     {/each}
   </div>
 {:catch}
@@ -65,4 +68,7 @@
     <p>查询相似路径失败...</p>
   </div>
 {/await}
-<p>或许你还可以通过 <a href="https://shenzilong.cn/关于/申子龙.html#我的联系方式">联系我</a> 来查找相关资料</p>
+<p>
+  或许你还可以通过 <a
+    href="https://shenzilong.cn/关于/申子龙.html#我的联系方式">联系我</a> 来查找相关资料
+</p>
