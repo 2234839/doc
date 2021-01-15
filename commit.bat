908227ab1f5749ed7e737387ee10a10344104811
崮生 <admin@shenzilong.cn>
2021-01-15 17:13:07 +0800

    ✨feat(blog build): 修改build 参数

--- commit.bat
+++ commit.bat
@@ -1,3 +1,5 @@
+scp C:/Users/llej/AppData/Local/Temp/siyuan/siyuan.db root@shenzilong.cn:/root/app/md2website
+
 cd D:/code/doc/
 git add .
 git commit -m "doc"
--- src/routes/blog/auto_update.ts
+++ src/routes/blog/auto_update.ts
@@ -13,7 +13,7 @@ export async function get(req: any, res: any) {
       console.log("[更新代码博客失败]", e);
     } else {
       console.log("[更新成功]");
-      exec(`../app/md2website/linux_md2website "/root/doc/doc" "./docHTML" "/root/app/md2website/views/"`, { cwd: root_path }, (e, stdout) => {
+      exec(`../app/md2website/linux_md2website "/root/doc/doc" "./docHTML" "/root/app/md2website/views/" "/root/app/md2website/siyuan.db"`, { cwd: root_path }, (e, stdout) => {
         if (e) {
           console.log("[编译html失败]", e);
         } else {
