06101525aff989e484d291516be048aa3bd11b41
崮生 <admin@shenzilong.cn>
2021-03-12 13:34:11 +0800

    doc

--- src/routes/blog/auto_update.ts
+++ src/routes/blog/auto_update.ts
@@ -13,13 +13,28 @@ export async function get(req: any, res: any) {
       console.log("[更新代码博客失败]", e);
     } else {
       console.log("[更新成功]");
-      exec(`../app/md2website/linux_md2website "/root/doc/doc" "./docHTML" "/root/app/md2website/views/" "/root/app/md2website/siyuan.db"`, { cwd: root_path }, (e, stdout) => {
-        if (e) {
-          console.log("[编译html失败]", e);
-        } else {
-          console.log("[编译html成功]");
-        }
-      });
+      exec(
+        `../app/md2website/linux_md2website "/root/doc/doc" "./docHTML" "/root/app/md2website/views/" "/root/app/md2website/siyuan.db"`,
+        { cwd: root_path },
+        (e, stdout) => {
+          if (e) {
+            console.log("[编译html失败]", e);
+          } else {
+            console.log("[编译html成功]", stdout);
+          }
+        },
+      );
+      exec(
+        `./ossutil64 sync -f -u ../doc/doc oss://store-llej/doc/`,
+        { cwd: "/root/app" },
+        (e, stdout) => {
+          if (e) {
+            console.log("[上传阿里云 oss 失败]", e);
+          } else {
+            console.log("[上传阿里云 oss 成功]", stdout);
+          }
+        },
+      );
       /** 去触发一次更新 */
       获取文档资源(true);
     }
