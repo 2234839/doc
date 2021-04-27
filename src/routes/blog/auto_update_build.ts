0b07740aa94aa57025d5df7a439c908287232707
崮生 <admin@shenzilong.cn>
2021-04-27 20:17:20 +0800

    修改 ci build 方式

--- src/routes/blog/auto_update_build.ts
+++ src/routes/blog/auto_update_build.ts
@@ -1,52 +1,10 @@
-import { exec } from "child_process";
-import type { IncomingMessage } from "http";
-import { root_path } from "../../lib/env";
-/** post 接口做预览之用 ldsfoiu9071384ohjfaiou149814 */
-export async function get(req: IncomingMessage, res: any) {
-  const data = [] as any[];
-  req.on("data", function (chunk) {
-    data.push(chunk);
-  });
-  req.on("end", async () => {
-    const payload = JSON.parse(data.join(""));
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-    if ((payload.ref as string).startsWith("refs/heads/gh-pages")) {
-      res.end(JSON.stringify({ code: 200 }));
-      /** 更新代码，刷新博客内容 */
-      exec(
-        "cd /root/doc/__sapper__ && " +
-          "rm -rf /root/doc/__sapper__/doc && " +
-          "git clone -b gh-pages --depth=1 git@github.com:2234839/doc.git && " +
-          "\\cp -rf ./doc/build/ ./ && " +
-          "pm2 reload blog",
-        { cwd: root_path },
-        (e, stdout) => {
-          if (e) {
-            console.log("[更新程序失败]", e);
-          } else {
-            console.log("[更新程序成功]");
-            exec(
-              `cd /root/doc/__sapper__/doc/ && \\cp -rf ./build/ ../build/ && pm2 reload blog`,
-              { cwd: root_path },
-              (e, stdout) => {
-                if (e) {
-                  console.log("[启动程序失败]", e);
-                } else {
-                  console.log("[启动程序成功]", stdout.toString());
-                }
-              },
-            );
-          }
-        },
-      );
-    } else {
-      console.log("[payload]", { ref: payload.ref, head_commit: payload.head_commit });
+import type { defaultHandle } from 'src/hooks';
 
-      return res.end(JSON.stringify({ code: 200 }));
-    }
-  });
-}
+export const get: defaultHandle = async function get() {
+	return {
+		status: 200,
+		body: {}
+	};
+};
 
 export const post = get;
