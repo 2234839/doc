0c515fabb009f6ccbe8bd4fa1eec17992c98587e
崮生 <admin@shenzilong.cn>
2024-06-03 17:15:45 +0800

    回退上传s3的代码

---
+++ upload_s3.ts
@@ -0,0 +1,47 @@
+import * as Minio from 'minio';
+import fs from "fs/promises"
+
+console.log(process.env.ENDPOINT);
+
+const minioClient = new Minio.Client({
+    endPoint: process.env.ENDPOINT!,
+    port: 443,
+    useSSL: true,
+    accessKey: process.env.ACCESSKEY!,
+    secretKey: process.env.SECRETKEY!,
+})
+const BucketName = "myjfs"
+
+const localRepoPath = "./docs"
+const remotePath = "app/docs"
+
+const NotFound = Symbol("NotFound")
+
+uploadDirToS3(remotePath,localRepoPath);
+
+// 读取本地git仓库的内容并上传到S3存储桶（增量上传）
+async function uploadDirToS3(s3Dir: string, localDir: string) {
+    const files = await fs.readdir(localDir);
+    await Promise.all(files.map(async name=>{
+        const s3Path = `${s3Dir}/${name}`
+        const localPath = `${localDir}/${name}`
+        const fileStat = await fs.stat(localPath)
+        if (fileStat.isDirectory()) {
+            await uploadDirToS3(s3Path,localPath)
+        } else {
+            const objectStat = await (minioClient.statObject(BucketName, s3Path).catch((e: Error) => {
+                if (e.message.includes("Not Found")) {
+                    return NotFound
+                }
+            }))
+            // 简单的认为文件大小不一致就不一样
+            if (objectStat === undefined || objectStat === NotFound || objectStat.size !== fileStat.size) {
+                minioClient.fPutObject(BucketName, s3Path, localPath, {}).then(r => {
+                    console.log("upload：", s3Path, `${(fileStat.size / 1024).toFixed(0)}k`, r);
+                })
+            } else {
+                console.log("跳过", s3Path);
+            }
+        }
+    }))
+};
