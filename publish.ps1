52fcfaec4b99e7fdf6d810601c735105f916f7f4
崮生 <admin@shenzilong.cn>
2024-12-27 14:32:18 +0800

    update docs

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
-cd D:/code/oceanPress_js/apps/frontend;
-pnpm cli build --config "./store/configs" --output "D:/code/doc/docs"
+cd D:/code/code/oceanPress_js/apps/frontend;
+pnpm cli build --config "./store/configs" --output "D:/code/code/doc/docs"
 
-cd D:/code/doc
+cd D:/code/code/doc
 git add .;
 git commit -m "update docs";
 git push;
