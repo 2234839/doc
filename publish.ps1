14755f633405a0df255db5259f44b3e43a711ed3
崮生（子虚） <2234839456@qq.com>
2025-03-15 21:31:15 +0800

    更新

--- publish.ps1
+++ publish.ps1
@@ -2,6 +2,7 @@ cd /home/gs/opensource_code/oceanpress/apps/frontend;
 pnpm cli build --config "./store/configs" --output "/home/gs/opensource_code/doc/docs"
 
 cd /home/gs/opensource_code/doc
-git add .;
-git commit -m "update docs";
-git push;
+# git add .;
+# git commit -m "update docs";
+# git push;
+pnpm upload
--- upload_s3.ts
+++ upload_s3.ts
@@ -1,6 +1,10 @@
 import * as Minio from 'minio';
 import fs from 'fs/promises';
-console.log(process.env.ENDPOINT);
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(process.env.ACCESSKEY);
 
 const minioClient = new Minio.Client({
   endPoint: process.env.ENDPOINT! || 's3.shenzilong.cn',
@@ -21,14 +25,18 @@ uploadDirToS3(remotePath, localRepoPath);
 // 读取本地git仓库的内容并上传到S3存储桶（增量上传）
 async function uploadDirToS3(s3Dir: string, localDir: string) {
   const files = await fs.readdir(localDir);
-  await Promise.all(
-    files.map(async (name) => {
-      const s3Path = `${s3Dir}/${name}`;
-      const localPath = `${localDir}/${name}`;
-      const fileStat = await fs.stat(localPath);
-      if (fileStat.isDirectory()) {
-        await uploadDirToS3(s3Path, localPath);
-      } else {
+  const concurrencyLimit = 5; // 设置并发限制为5
+  const uploadQueue: Promise<void>[] = [];
+
+  for (const name of files) {
+    const s3Path = `${s3Dir}/${name}`;
+    const localPath = `${localDir}/${name}`;
+    const fileStat = await fs.stat(localPath);
+
+    if (fileStat.isDirectory()) {
+      await uploadDirToS3(s3Path, localPath);
+    } else {
+      const uploadTask = async () => {
         const objectStat = await minioClient.statObject(BucketName, s3Path).catch((e: Error) => {
           if (e.message.includes('Not Found')) {
             return NotFound;
@@ -46,7 +54,19 @@ async function uploadDirToS3(s3Dir: string, localDir: string) {
         } else {
           console.log('跳过', s3Path);
         }
+      };
+
+      uploadQueue.push(uploadTask());
+
+      if (uploadQueue.length >= concurrencyLimit) {
+        await Promise.all(uploadQueue);
+        uploadQueue.length = 0; // 清空队列
       }
-    }),
-  );
+    }
+  }
+
+  // 处理剩余的任务
+  if (uploadQueue.length > 0) {
+    await Promise.all(uploadQueue);
+  }
 }
